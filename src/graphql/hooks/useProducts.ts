3c994f5af5c13a7510cd347206884ac78f500fb3
SyedMinhajAlbeez <147368067+SyedMinhajAlbeez@users.noreply.github.com>
2026-02-23 16:13:20 +0500

    updated fixed
    updated fixed

--- src/graphql/hooks/useProducts.ts
+++ src/graphql/hooks/useProducts.ts
@@ -1,18 +1,6 @@
 import { useQuery, type QueryHookOptions, type QueryResult } from "@apollo/client";
 import { GET_PRODUCTS } from "../catelog/queries/Product";
-
-export interface GetProductsVariables {
-  query?: string;
-  sortKey?: string;
-  reverse?: boolean;
-  first?: number;
-  last?: number;
-  after?: string;
-  before?: string;
-  channel?: string;
-  locale?: string;
-  filter?: string;
-}
+import type { GetProductsVariables } from "../types";
 
 export function useProductsQuery<TData = any>(
   variables: GetProductsVariables,
@@ -23,4 +11,3 @@ export function useProductsQuery<TData = any>(
     variables,
   });
 }
-
